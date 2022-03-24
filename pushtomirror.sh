fea0ac3e1e4485b31349832d465eecf9a8a61b04
Neruthes <i@neruthes.xyz>
2022-03-24 19:41:14 +0000

    Feature: Server-side tarball creation

--- pushtomirror.sh
+++ pushtomirror.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 rsync -av --delete _dist/ repo.aosc.io:/mirror/misc/artifacts/miscdoc/
+ssh repo.aosc.io 'cd /mirror/misc/artifacts; tar vcf /mirror/misc/artifacts.tar/miscdoc.tar miscdoc'
+
+
+
+
+#######################################################################
+
+
+
+
+### Verify tarball content:
+# cd /mirror/misc/artifacts.tar; ls; du -h miscdoc.tar; tar pxvf miscdoc.tar; tree;
+# rm -rf /mirror/misc/artifacts.tar/miscdoc*
