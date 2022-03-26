64ec3f750e365c876f15aedced5f9056fe0dc3d8
Neruthes <i@neruthes.xyz>
2022-03-26 00:54:20 +0000

    release.sh: Hello world

---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm miscdoc.tar.xz 2>/dev/null
+cat README.md > _dist/README.md
+
+### Create directories
+mkdir -p _dist/_Markdown
+for PROJ in $(cat projlist); do
+    mkdir -p "_dist/_Markdown/$PROJ"
+done
+
+### Save Markdown files
+for READMEMDPATH in */*/README.md; do
+    DOCNAMEDIR="$(dirname "$READMEMDPATH")"
+    DESTPATH="_dist/_Markdown/$DOCNAMEDIR.md"
+    # echo "Saving '$READMEMDPATH' to '$DESTPATH'"
+    cat "$READMEMDPATH" > "$DESTPATH"
+done
+
+### Generate tarball
+tar --xz -cf miscdoc.tar.xz _dist
+
+### Final notes
+echo "Created release file 'miscdoc.tar.xz'"
+echo "The release version should be '$(date +'%Y%m%d')'"
